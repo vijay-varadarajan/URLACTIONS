2fdc21e8bd07213e151232b86cc1716978504bbb
Vijay Varadarajan <vijayvaradarajan84@gmail.com>
2024-12-31 00:09:58 +0530

    add command-line flags for URL shortening and QR code generation

--- command.go
+++ command.go
@@ -1,13 +1,30 @@
 package main
 
-import (
-	"flag"
-	"fmt"
-	"os"
-	"strconv"
-	"strings"
-)
+import "flag"
+
+// import (
+// 	"flag"
+// 	"fmt"
+// 	"os"
+// 	"strconv"
+// 	"strings"
+// )
 
 type cmdFlags struct {
-	
+	shorten string
+	qrcode string 
+	save string
 }
+
+func newCmdFlag() *cmdFlags {
+	cf := cmdFlags{} 
+
+	flag.StringVar(&cf.shorten, "shorten", "", "Shorten the provided url string")
+	flag.StringVar(&cf.qrcode, "qrcode", "", "Convert the provided url to a qrcode")
+	flag.StringVar(&cf.save, "save", "", "Save the qrcode in given filename")
+
+	flag.Parse()
+
+	return &cf
+}
+
--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module url-cli
 go 1.23.4
 
 require (
-	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e // indirect
-	github.com/subosito/shorturl v1.0.0 // indirect
+	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e
+	github.com/subosito/shorturl v1.0.0
 )
